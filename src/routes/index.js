9eed9ff6286e9596e6b5905edd197698935ffe2c
AgustinMO01 <agmaol@alumnatflorida.es>
2025-02-09 16:04:43 +0100

    refactorizacion funcion flecha 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 //Rutas GET
-app.get("/rutaGEt", (req, res) => {
+router.get("/rutaGEt", (req, res) => {
     console.log("hola en la consola de node, desde la 1ª ruta GET en el servidor");
     res.send("hola soy la 1ª ruta GET");
 })
