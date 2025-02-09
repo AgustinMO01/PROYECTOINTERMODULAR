e8d9a7de160f75e983113947f15de06420ffd8fa
AgustinMO01 <agmaol@alumnatflorida.es>
2025-02-09 15:46:12 +0100

    creacion de rutas

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), leerPuerto);
 
+app.get("/rutaGEt", function (req, res) {
+    console.log("hola en la consola de node, desde una llamada en el servidor");
+    res.send("hola desde una web llamada en el servidor");
+})
+
 function leerPuerto() {
     console.log("hola desde el puerto " + app.get("port"));
 }
