c3600ba9a6bbd64083ca99670148f8e4bcade096
AgustinMO01 <agmaol@alumnatflorida.es>
2025-02-09 14:59:18 +0100

    final de la entrega de github con cambios en repositorio

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 
 console.log("Ahora empieza el proceso para llevar a cabo el proyecto");
 console.log("Ahora podemos ver que los cambios se actualizan correctamente.");
+console.log("Final de la entrega de github de proyecto intermodular");
+console.log("Se pueden ver todos los cambios");
