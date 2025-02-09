82b096542da976f0a5eed7e40e85a64fe9577760
AgustinMO01 <agmaol@alumnatflorida.es>
2025-02-09 15:53:06 +0100

    refactorizando el código

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,9 @@ router.get("/rutaGetConRouter", function (req, res) {
     res.send("hola desde una web llamada en el servidor utilizando routes");
 });
 
+app.get("/rutaGEt", function (req, res) {
+    console.log("hola en la consola de node, desde una llamada en el servidor");
+    res.send("hola desde una web llamada en el servidor");
+})
+
 module.exports = router;
