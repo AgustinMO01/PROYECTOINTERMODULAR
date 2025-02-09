c63692481d786deb7f1b9b5fdd62effbe206e8a4
AgustinMO01 <agmaol@alumnatflorida.es>
2025-02-09 16:08:31 +0100

    response en formato JSON

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,5 +12,12 @@ router.get("/rutaGetConRouter", (req, res) => {
     res.send("hola soy la 2ª ruta GET");
 });
 
+router.get("rutaGetConJSON", (req, res) => {
+    console.log("hola en la consola de node, desde la 3ª ruta GET en el servidor");
+    res.json({
+        Title: "Hola mundo con express probando cosas para subir a github",
+    });
+});
+
 
 module.exports = router;
