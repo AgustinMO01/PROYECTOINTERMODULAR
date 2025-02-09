8c72d42ba4c1741aaa37961b40c58ef32a4a3f36
AgustinMO01 <agmaol@alumnatflorida.es>
2025-02-09 14:57:11 +0100

    comprobación de cambios

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log("Esto es mi primera modificacion en un repositorio.");
 console.log("Ahora empieza el proceso para llevar a cabo el proyecto");
+console.log("Ahora podemos ver que los cambios se actualizan correctamente.");
